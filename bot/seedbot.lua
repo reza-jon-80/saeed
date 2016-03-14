d0df92917d13baef7e3a49a63a5f0c26bf6667fa
reza-jon-80 <reza09190857541@gmail.com>
2016-03-14 23:53:49 +0330

    up

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {198828890,145266153},--Sudo users
+    sudo_users = {112707993,198488095},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[W_SaTaN_W V 1
